19aebf7a77edc1b62bdecf00e2436f48d06bff97
sg21 <sg21@shrutis-air.wireless-1x.unc.edu>
2019-09-01 21:02:41 -0400

    geqrjkfasdnlkcmHJLGASDFKJLK

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,14 +1,20 @@
 package a1;
 
+
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class A1Jedi {
 
+	static Scanner scan = new Scanner(System.in);
+
+	
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
-	}
+	
 }
+
+}
+
