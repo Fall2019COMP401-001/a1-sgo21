49ec4a2d7bd1b2557ed3ec0a7b69133e39c0a5a0
sgo21 <sg21@live.unc.edu>
2019-09-01 23:44:05 -0400

    im comitting/pushing from my laptop (sg21)?? fixed authors to sgo21

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,12 +7,12 @@ import java.util.HashMap;
 
 public class A1Jedi {
 
-	static Scanner scan = new Scanner(System.in);
 
-	
 	public static void main(String[] args) {
 		
-
+		Scanner scan = new Scanner(System.in);
+		
+		
 	
 }
 
